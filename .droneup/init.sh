49481b5b1d06dff421588ef2c610e316b851bb51
Charles Russell <charles.russell@webteks.com>
2018-10-26 19:15:16 +0000

    Messed up interupter.  Envkey now required on run start, and will merge variables

--- .droneup/init.sh
+++ .droneup/init.sh
@@ -1,4 +1,4 @@
-#!bin/bash
-
+#!/bin/bash
+echo "Initializing"
 [ -z "$ENVKEY" ] && echo "ENVKEY wasn't provided at run time, please include -e ENVKEY=\"details\" to docker!" && exit 1 || echo " -- Merge Environment variables"
-eval $(envkey-source $ENVKEY) && "@$"
+eval $(envkey-source $ENVKEY) && eval "$@"
